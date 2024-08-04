f7145003e1b190bf54577d625912fae0e1d7ff49
Hadeel Abdellatif <hadeel.abdellatif2001@gmail.com>
2024-08-04 19:06:00 +0300

    Add project files.

---
+++ Entity Framework Task 2/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Entity_Framework_Task_2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framework_Task_2.Data
+{
+    internal class ApplicationDbContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.; Database=products_task2; Trusted_Connection=true; TrustServerCertificate=true;");
+        }
+    }
+}
